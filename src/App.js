6cb9efc4c40a591df47d7b24e35d729cd40e6d03
bearchit <bearchit@gmail.com>
2019-10-29 18:22:16 +0900

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
 function App() {
-  return <div>Hello, world</div>;
+  return <Typography variant="h1">Hello, world</Typography>;
 }
 
 export default App;
