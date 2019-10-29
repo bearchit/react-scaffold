03a7d4f569d83e0d814788834f39a852d4105d78
bearchit <bearchit@gmail.com>
2019-10-29 19:32:24 +0900

    responsive meta tag

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { Typography } from '@material-ui/core';
+import { CssBaseline, Typography } from '@material-ui/core';
 import React from 'react';
 
 function App() {
-  return <Typography variant="h1">Hello, world</Typography>;
+  return (
+    <div>
+      <CssBaseline />
+      <Typography variant="h1">Hello, world</Typography>
+    </div>
+  );
 }
 
 export default App;
